0c5af4932738443a96831d05016d1cacde906742
C.T <charlestbell@gmail.com>
2023-08-06 21:47:12 -0400

    feat:

---
+++ gatsby-node.js
@@ -0,0 +1,69 @@
+const axios = require("axios");
+
+exports.createPages = async ({ actions: { createPage } }) => {
+  const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+  const posts = res.data;
+
+  posts.forEach((post) => {
+    createPage({
+      path: `/posts/${post.id}`,
+      component: require.resolve("./src/templates/post.js"),
+      context: { post },
+    });
+  });
+
+  createPage({
+    path: "/posts",
+    component: require.resolve("./src/templates/posts.js"),
+    context: { posts },
+  });
+};
+
+exports.createSchemaCustomization = ({ actions }) => {
+  const { createTypes } = actions;
+  const typeDefs = `
+type PostJson {
+  id: ID!
+title: String
+body: String
+}
+
+input TitleFilter {
+  eq: String
+  in: String
+}
+`;
+  createTypes(typeDefs);
+};
+
+exports.createResolvers = ({ createResolvers }) => {
+  const resolvers = {
+    Query: {
+      allPosts: {
+        args: {
+          filter: `input PostFilterInput {title: TitleFilter}`,
+          limit: "Int",
+        },
+        type: ["PostJson"],
+        async resolve(source, { filter }, context, info) {
+          const { title } = filter || {};
+          const { eq } = title || {};
+          console.log("Hello from allPosts resolver");
+
+          const res = await axios.get(
+            "https://jsonplaceholder.typicode.com/posts"
+          );
+          const posts = res.data;
+          console.log("POSTS", posts);
+
+          if (filter) {
+            return posts.filter((post) => post.title === eq);
+          }
+          return posts;
+        },
+      },
+    },
+  };
+
+  createResolvers(resolvers);
+};
---
+++ src/pages/blogs.js
@@ -0,0 +1,12 @@
+
+import React from "react"
+import Layout from "../components/Layout"
+
+export default function Blogs() {
+
+  return (
+    <Layout>
+      <h1>I am blogs page!</h1>
+    </Layout>
+  )
+}
